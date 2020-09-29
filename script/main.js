8e0052156e9e95b85da9009d195a8f769dad8e33
Vladymyr Ivanchenko <vladymyr_ivanchenko@bigmir.net>
2020-09-29 22:44:19 +0300

    добавляет бургер-меню и генерацию хедера, футера и меню для всех файлов хтмл с помощью js

---
+++ script/main.js
@@ -0,0 +1,42 @@
+"use sctrict";
+
+const btnBurger = document.querySelector(".btn-burger");
+const catalog = document.querySelector(".catalog");
+const btnClose = document.querySelector(".btn-close");
+const subCatalog = document.querySelector(".subcatalog");
+const subCatalogHeader = document.querySelector(".subcatalog-header");
+const btnReturn = document.querySelector(".btn-return");
+
+const overlay = document.createElement("div");
+overlay.classList.add("overlay");
+document.body.insertAdjacentElement("beforeend", overlay);
+
+const closeMenu = () => {
+  catalog.classList.remove("open");
+  overlay.classList.remove("active");
+  closeSubMenu();
+};
+
+const openMenu = () => {
+  catalog.classList.add("open");
+  overlay.classList.add("active");
+};
+
+const openSubMenu = (event) => {
+  event.preventDefault();
+  const itemList = event.target.closest(".catalog-list__item");
+  if (itemList) {
+    subCatalogHeader.innerHTML = itemList.innerHTML;
+    subCatalog.classList.add("subopen");
+  }
+};
+
+const closeSubMenu = () => {
+  subCatalog.classList.remove("subopen");
+};
+
+btnBurger.addEventListener("click", openMenu);
+btnClose.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
+catalog.addEventListener("click", openSubMenu);
+btnReturn.addEventListener("click", closeSubMenu);
